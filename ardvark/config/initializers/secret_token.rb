f1bc621fe29f9fe58343ad3e933071ac4aa4199d
Sam Lachance <slachance@gmail.com>
2017-03-03 00:59:37 -0500

    rails 3.2.9 (xml/yaml remote code exec)

---
+++ ardvark/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorXmlYamlCodeExecution::Application.config.secret_token = '5d235fa935be5b99986b39fd78e40072dee3e2bb001fba25766b627c4fc579906365d98340dc9b3bd237fb6d4882ac3e0a3ff3d2977187375fb0ae2599e51287'
---
+++ ardvark/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RorXmlYamlCodeExecution::Application.config.session_store :cookie_store, key: '_ror_xml_yaml_code_execution_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RorXmlYamlCodeExecution::Application.config.session_store :active_record_store
